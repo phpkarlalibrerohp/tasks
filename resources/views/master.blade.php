a8ce47fac552ba58da30ab8e208f80f1461510db
Karla Librero <karla.l@hpoutsourcinginc.com>
2015-06-10 12:02:19 +0800

    CSRF Token Broken

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="_token" content="{{ csrf_token() }}" />
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Laravel</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
@@ -65,11 +65,11 @@
 	<script>
 		$(document).ready(function() {
 			$(function() {
-			    $.ajaxSetup({
+			   $.ajaxSetup({
 			        headers: {
-			            'X-CSRF-Token': $('meta[name="_token"]').attr('content')
+			            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 			        }
-			    });
+				});
 			});
 		});
 	</script>
